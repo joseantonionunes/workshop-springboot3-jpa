63ecfbf42a942d7c68a9443173353013b8a3022b
joseantonionunes <151632189+joseantonionunes@users.noreply.github.com>
2024-05-06 19:13:16 -0300

    Exception handling - delete

--- src/main/java/com/aprendendospring/treino/services/UserService.java
+++ src/main/java/com/aprendendospring/treino/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.aprendendospring.treino.entities.User;
 import com.aprendendospring.treino.repositories.UserRepository;
+import com.aprendendospring.treino.services.exceptions.DatabaseException;
 import com.aprendendospring.treino.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long obj) {
-		repository.deleteById(obj);
+		try {
+			repository.deleteById(obj);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(obj);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
